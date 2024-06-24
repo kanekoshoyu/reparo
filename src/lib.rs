103224b7694ac993eff1f9988a50070037a87f86
Sho Kaneko <kanekoshoyu@gmail.com>
2024-06-24 16:15:27 +0800

    unit test to do read write

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::NaiveDateTime;
+use chrono::{DateTime, Utc};
 use kanal::AsyncSender;
 use std::{
     fs::File,
@@ -8,7 +8,8 @@ use tokio::time::{sleep_until, Duration, Instant};
 
 /// any event that is to be rewinded should be rewindable
 pub trait Rewindable: serde::Serialize + for<'d> serde::Deserialize<'d> {
-    fn timestamp(&self) -> chrono::NaiveDateTime;
+    // timestamp
+    fn timestamp(&self) -> chrono::DateTime<chrono::Utc>;
 }
 
 pub fn save_vec_to_file<T: Rewindable>(vec: &Vec<T>, file_path: &str) -> std::io::Result<()> {
@@ -37,16 +38,15 @@ pub async fn rewind_broadcast<T: Rewindable>(
     if events.is_empty() {
         return;
     }
-    // TODO sort events by timestamp
     events.sort_by_key(|x| x.timestamp());
     // get offset
-    let time_now = chrono::Utc::now().naive_utc();
+    let time_now = chrono::Utc::now();
     // await per next offset up, until the events get used up
     let first_rewindable_data = events.first().unwrap();
     let time_offset = first_rewindable_data.timestamp() - time_now;
     for event in events {
         // simplify await logic with the use of condvar
-        // await until the timee has passed
+        // await until the time has passed
         // simply set an await until which use the offset
         let time_next_event = event.timestamp() + time_offset;
         wait_until_timestamp(time_next_event).await;
@@ -57,8 +57,8 @@ pub async fn rewind_broadcast<T: Rewindable>(
     }
 }
 
-pub async fn wait_until_timestamp(time_target: NaiveDateTime) {
-    let time_now = chrono::Utc::now().naive_utc();
+pub async fn wait_until_timestamp(time_target: DateTime<Utc>) {
+    let time_now = chrono::Utc::now();
     if time_target > time_now {
         let wait_duration = time_target - time_now;
         let wait_duration = Duration::from_micros(wait_duration.num_microseconds().unwrap() as u64);
@@ -67,23 +67,49 @@ pub async fn wait_until_timestamp(time_target: NaiveDateTime) {
     }
 }
 
-// TODO implement subscribe and store into file
-
 // TODO implement parse file unit test
 
 mod tests {
+
     /// verify that the value can be read/write
     #[tokio::test]
     async fn test_write_read() {
+        use crate::Rewindable;
+        use serde::{Deserialize, Serialize};
+        use std::io::{Read, Write};
+        use tempfile::tempdir;
+
         // set up cache connection
+        #[derive(Serialize, Deserialize, PartialEq, Debug)]
         struct TestStruct {
             data: String,
             timestamp: chrono::DateTime<chrono::Utc>,
+        }
+        impl Rewindable for TestStruct {
+            fn timestamp(&self) -> chrono::DateTime<chrono::Utc> {
+                self.timestamp
+            }
         }
         let test_struct = TestStruct {
             data: "Test".to_string(),
             timestamp: chrono::Utc::now(),
         };
-        //serialize
+        // serialize to a JSON string
+        let json_str = serde_json::to_string(&test_struct).unwrap();
+        println!("Serialized JSON: {}", json_str);
+
+        let dir = tempdir().unwrap();
+        let path = dir.path().join("test.json");
+        let mut file = std::fs::File::create(path.clone()).unwrap();
+        file.write_all(json_str.as_bytes()).unwrap();
+
+        // read back the file content
+        let mut read_json_str = String::new();
+        let mut file = std::fs::File::open(&path).unwrap();
+        file.read_to_string(&mut read_json_str).unwrap();
+
+        // Deserialize the JSON string back to a Person struct
+        let read_struct: TestStruct = serde_json::from_str(&read_json_str).unwrap();
+        assert_eq!(read_struct, test_struct)
     }
 }
